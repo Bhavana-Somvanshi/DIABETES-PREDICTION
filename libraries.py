71d9f7fd173ae8c20c2cfbdfdc554a83eb1d788e
Bhavana Singh <100597557+Bhavana-Somvanshi@users.noreply.github.com>
2023-05-13 22:04:02 +0530

    code

---
+++ libraries.py
@@ -0,0 +1,13 @@
+Importing the Libraries
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from collections import Counter
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+import pickle
